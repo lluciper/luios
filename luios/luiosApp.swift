43f08883de9c7d6ba7b266812e14aba57361cd3a
Phuong Hoang <phuonghoang9910@gmail.com>
2023-07-21 09:41:42 +0700

    Initial Commit

---
+++ luios/luiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  luiosApp.swift
+//  luios
+//
+//  Created by Who care on 21/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct luiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
